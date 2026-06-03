ce3b30bf631f56ebbf97fda0b1a492b13684f442
Gustavo4681501 <matag468@gmail.com>
2026-06-03 10:02:37 -0600

    feat: wrap app with ToastProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
+// src/App.jsx
 import { BrowserRouter, Routes, Route } from "react-router-dom"
-
+import { ToastProvider } from "./components/Toast"
 import ActivoDetallePage from "./pages/activoDetallePage/ActivoDetallePage"
 import Home from "./pages/home/Home"
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* Ruta para ver detalles y agregar propietarios */}
-        <Route path="/activo/:id" element={<ActivoDetallePage />} />
-      </Routes>
-    </BrowserRouter>
+    <ToastProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/activo/:id" element={<ActivoDetallePage />} />
+        </Routes>
+      </BrowserRouter>
+    </ToastProvider>
   )
 }
 
